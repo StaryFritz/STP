5f43679ef4d0d3a4d0bf5bf3f542743c2eeb06ea
StaryFritz <mihail.tennis@gmail.com>
2022-02-14 18:50:07 +0300

    labChange

--- src/helloworld.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        // Display "Hello World!"
-        System.out.println("Hello World!");
-        //TODO Add a new method
-    }
-}
